02be3083a33bf2eac9329e6d4def3104e0ef5398
kmein <kieran.meinhardt@gmail.com>
2016-08-01 08:54:23 +0200

    Add getMe example

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+module Lib where
+
+import Network.HTTP.Client (newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Web.Telegram.API.Bot
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+getMeExample =
+    do manager <- newManager tlsManagerSettings
+       res <- getMe token manager
+       case res of
+         Left e ->
+             do putStrLn "Request failed"
+                print e
+         Right GetMeResponse { user_result = u } ->
+             do putStrLn "Request succeeded"
+                print $ user_first_name u
+    where token = Token $ "bot" <> token
+
