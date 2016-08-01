a04756107a43670fc7b57bc86b0d4b7c7469e56e
kmein <kieran.meinhardt@gmail.com>
2016-08-01 09:20:53 +0200

    Add polling and utility functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lib where
 
-import Network.HTTP.Client (newManager)
+import Data.Monoid
+import Network.HTTP.Client (newManager, Manager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Web.Telegram.API.Bot
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+token = Token $ "bot" <> "252445649:AAH3NQZeVQRvZD-b860REZgvBJ8Jo9M_wPM"
 
 getMeExample =
     do manager <- newManager tlsManagerSettings
@@ -18,5 +21,13 @@ getMeExample =
          Right GetMeResponse { user_result = u } ->
              do putStrLn "Request succeeded"
                 print $ user_first_name u
-    where token = Token $ "bot" <> token
 
+pollMessages :: Token -> Manager -> IO (Maybe UpdatesResponse)
+pollMessages token manager =
+    eitherToMaybe <$> getUpdates token Nothing Nothing Nothing manager
+
+eitherToMaybe :: Either a b -> Maybe b
+eitherToMaybe = const Nothing `either` Just
+
+handleMaybe :: Maybe a -> b -> (a -> b) -> b
+handleMaybe = flip (flip . maybe)
