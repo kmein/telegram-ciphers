cdc9f792d4263a32e9f7356018b6329ed439d9e5
kmein <kieran.meinhardt@gmail.com>
2016-08-03 19:57:55 +0200

    Improve error message if token file does not exist

--- app/Main.hs
+++ app/Main.hs
@@ -13,5 +13,6 @@ main =
                 exists <- doesFileExist absPath
                 if exists
                 then run absPath
-                else putStrLn "The file you specified does not exist."
+                else putStrLn "The file you specified does not exist.\n\
+                    \(Note: home directory (tilde) expansion is not supported yet.)"
          _ -> putStrLn "Invalid arguments. Supply one file path, please."
